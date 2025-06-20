60dcf5a9d144509fe507c800a7d44f7d6354dd2d
SeungWon <semo970921@naver.com>
2025-06-20 16:44:53 +0900

    feat/헤더영역

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import Header from "./components/Header/Header";
 
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main className="max-w-[1440px] mx-auto px-6 py-8"></main>
+    </div>
+  );
+}
 export default App;
