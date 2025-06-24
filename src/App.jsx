38c1f6d32e583739f9eb091ca0d0e51591167418
SeungWon <semo970921@naver.com>
2025-06-24 15:16:30 +0900

    나의 플랜 생성시 사용해야 하는 공통모듈 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
 import PrivacyPolicy from "./components/Footer/PrivacyPolicy";
 import EmailCollectionPolicy from "./components/Footer/EmailCollectionPolicy";
 import TermsOfService from "./components/Footer/TermsOfService";
 import CustomerCenter from "./components/Footer/CustomerCenter";
-
-// 일단 공통모듈 테스트용
-import StepButton from "./components/common/MyPlan/StepButton";
-//import Step from "./components/common/MyPlan/Step";
-import StepIndicator from "./components/common/MyPlan/StepIndicator";
+import StepIndicator from "./components/common/MyPlan/Step";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-
-      <div className="flex gap-4 mb-8">
-        <StepButton type="prev" onClick={() => alert("이전으로")}>
-          이전단계
-        </StepButton>
-        <StepButton type="next" onClick={() => alert("다음으로")}>
-          다음단계
-        </StepButton>
-      </div>
 
       <main className="max-w-[1440px] mx-auto px-6 py-8">
         <Routes>
@@ -36,7 +22,7 @@ function App() {
           />
           <Route path="/terms-of-service" element={<TermsOfService />} />
           <Route path="/customer-service" element={<CustomerCenter />} />
-          <Route path="/" element={<StepIndicator />} />
+          <Route path="/myplan" element={<StepIndicator />} />
         </Routes>
       </main>
 
